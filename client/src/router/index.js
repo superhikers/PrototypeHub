87e009f7221add9a4c3280b49580a0c4f783650a
hikers <lb20920@vip.qq.com>
2026-06-06 17:56:28 +0800

    fix: 路由不存在时跳转首页

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,6 +5,7 @@ const routes = [
   { path: '/', name: 'projects', component: ProjectList },
   { path: '/project/:id', name: 'project-detail', component: () => import('../pages/ProjectDetail.vue') },
   { path: '/project/:id/settings', name: 'settings', component: () => import('../pages/Settings.vue') },
+  { path: '/:pathMatch(.*)*', redirect: '/' },
 ]
 
 export default createRouter({
